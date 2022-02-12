bfd8bf85f2d8a9ded7084e7e5ad8193de91a2d27
Elaine Cruz <e.cristina00@gmail.com>
2022-02-11 23:25:48 -0300

    trabalhando com pontos flutuantes

---
+++ src/com/sintaxe/variaveis/fluxo/TestePontoFlutuante.java
@@ -0,0 +1,20 @@
+package com.sintaxe.variaveis.fluxo;
+
+public class TestePontoFlutuante {
+    public static void main(String[] args) {
+        double salario = 3622.45; // exemplo de declaração de variáveis com ponto flutuante.
+        System.out.println("Meu salário é " + salario);
+
+        // int idade = 0.0 - o int não aceita casas decimais
+        // Toda variável é obrigatória ser declarada.
+
+        int primeiroNumero = 24;
+        int segundoNumero = 6 ;
+        int divisao = primeiroNumero / segundoNumero;
+
+        System.out.println(primeiroNumero);
+        System.out.println(segundoNumero);
+        System.out.println(divisao);
+
+    }
+}
