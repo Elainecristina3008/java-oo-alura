a1f8a4f96b7e7bae8de88fbaa247ab7f82fa39df
Elaine Cruz <e.cristina00@gmail.com>
2022-02-11 23:17:29 -0300

    Começando a trabalhar com variáveis

---
+++ src/com/sintaxe/variaveis/fluxo/TestaVariaveis.java
@@ -0,0 +1,15 @@
+package com.sintaxe.variaveis.fluxo;
+
+public class TestaVariaveis {
+    public static void main(String[] args) {
+        System.out.println("Olá novo teste!");
+        // Tota variável deve ser declarada.
+
+        int idade;
+        idade = 35;
+        // int idade = 35 - também pode ser declarada dessa forma.
+        System.out.println(idade);
+        System.out.println("Minha idade é: " + idade); // exemplo de concatenação.
+
+    }
+}
