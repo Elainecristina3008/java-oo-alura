22331d707b8895e3e1c157effc4c86ae4f37fdbf
Elaine Cruz <e.cristina00@gmail.com>
2022-02-11 22:53:36 -0300

    CCriando o Hello World

---
+++ src/com/principal/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.principal;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Minha segunda linha de código");
+    }
+
+}
