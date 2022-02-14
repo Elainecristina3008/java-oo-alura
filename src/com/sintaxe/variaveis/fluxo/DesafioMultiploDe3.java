fda12f0ea5bed35d7fd1bd54cfcee4042345b9aa
Elaine Cruz <e.cristina00@gmail.com>
2022-02-14 00:53:15 -0300

    Resolvendo desafio utilizando laços de repetição e estrutura condicional

---
+++ src/com/sintaxe/variaveis/fluxo/DesafioMultiploDe3.java
@@ -0,0 +1,11 @@
+package com.sintaxe.variaveis.fluxo;
+
+public class DesafioMultiploDe3 {
+    public static void main(String[] args) {
+        for (int numero = 1; numero <= 100; numero ++){
+            if(numero % 3 == 0){
+                System.out.println(numero + " é multiplo de 3 entre os numeros 1 e 100");
+            }
+        }
+    }
+}
