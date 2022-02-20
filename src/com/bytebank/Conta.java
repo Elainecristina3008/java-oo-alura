2d4d17c66a947b0b6ad11fd75b025060c49b4c8e
Elaine Cruz <e.cristina00@gmail.com>
2022-02-19 23:13:13 -0300

    Crfiando o método sacar

--- src/com/bytebank/Conta.java
+++ src/com/bytebank/Conta.java
@@ -10,4 +10,14 @@ public class Conta {
           this.saldo = this.saldo + valor;
      }
 
+     public boolean sacar(double valor){
+          if (this.saldo >= valor){
+               this.saldo = this.saldo - valor;
+               return true;
+          }else{
+               return false;
+          }
+
+     }
+
 }
--- src/com/bytebank/TestaMetodo.java
+++ src/com/bytebank/TestaMetodo.java
@@ -6,6 +6,15 @@ public class TestaMetodo {
         contaDaElaine.saldo = 200.00;
         contaDaElaine.deposita(150.00);
         System.out.println("O saldo atual é " + contaDaElaine.saldo);
+        System.out.println();
+        System.out.println();
+        System.out.println("------------------------------------------------------------------");
+        System.out.println();
+        System.out.println();
+
+        boolean conseguiuRetirar = contaDaElaine.sacar(20.50);
+        System.out.println(contaDaElaine.saldo);
+        System.out.println(conseguiuRetirar);
 
     }
 }
