827d17293eeeb755090ba141d79e06d75269def3
Elaine Cruz <e.cristina00@gmail.com>
2022-03-04 20:56:15 -0300

    Melhorando o cpolimorfismo

--- src/com/bytebankencapsulado/TesteReferencias.java
+++ src/com/bytebankencapsulado/TesteReferencias.java
@@ -2,14 +2,28 @@ package com.bytebankencapsulado;
 
 public class TesteReferencias {
     public static void main(String[] args) {
-        Funcionario gerente1 = new Gerente();
-
+        Gerente gerente1 = new Gerente();
+        //lado esquerdo é a referência e do lado direito o tipo
         gerente1.setNome("Elaine");
         String nome = gerente1.getNome();
-
+        gerente1.setSalario(2000.0);
         System.out.println(nome);
 
+        Funcionario funcionario1 = new Funcionario();
+        funcionario1.setSalario(1000.0);
+
+        EditorVideo ev = new EditorVideo();
+        ev.setSalario(1500.0);
+
+
         // gerente1.autentica(1551); Não funciona pois o método autentica é exclusivo da classe gerente e não de funcionário.
+
+        ControleBonificacao controle = new ControleBonificacao();
+        controle.registra(gerente1);
+        controle.registra(funcionario1);
+        controle.registra(ev);
+
+        System.out.println(controle.getSoma());
 
 
     }
